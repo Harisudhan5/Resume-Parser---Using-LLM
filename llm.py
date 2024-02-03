b10adf5dd0907f9c761fcbfa8b7f2814eb1b605e
Harisudhan.S <93301637+Harisudhan5@users.noreply.github.com>
2024-02-03 23:14:47 +0530

    prompt changed

--- llm.py
+++ llm.py
@@ -1,7 +1,6 @@
 from langchain_community.llms import Ollama
 
 llm = Ollama(model="llama2")
-
-p = llm.invoke("Get the Name, Phone no, github, linkedin, from the following content and return it as json format")
-
-print(p)
+prompt = ""
+result = llm.invoke("Get the Name, Phone no, github, linkedin, from the following content and return it as json format")
+print(result)
